72f2872bc8a02e8106376ef6614ec28d65db68fc
Colin Cannon <colincannon@kylibos.com>
2019-04-09 15:10:03 -0400

    added an if statement to conditionally connect to dev/staging firebase

--- app/src/firebase.js
+++ app/src/firebase.js
@@ -28,16 +28,31 @@ export {
 };
 */
 
+
   export const firebase = window.firebase;
 
   // Initialize Firebase
   // TODO: Replace with your project's customized code snippet
-  var config = {
-		apiKey: "AIzaSyBwLD9Zz3OgXt-3D7jex8cfd0F3prp_G54",
-		authDomain: "golf-meta-dev.firebaseapp.com",
-		databaseURL: "https://golf-meta-dev.firebaseio.com",
-		projectId: "golf-meta-dev",
-		storageBucket: "golf-meta-dev.appspot.com",
-		messagingSenderId: "823322072217"
-  };
+  	if (window.location.hostname == '127.0.0.1'){
+  		console.log('DEV FIREBASE');
+		var config = {
+			apiKey: "AIzaSyBwLD9Zz3OgXt-3D7jex8cfd0F3prp_G54",
+			authDomain: "golf-meta-dev.firebaseapp.com",
+			databaseURL: "https://golf-meta-dev.firebaseio.com",
+			projectId: "golf-meta-dev",
+			storageBucket: "golf-meta-dev.appspot.com",
+			messagingSenderId: "823322072217"
+		};
+	} else if (window.location.hostname == 'golf-meta-staging.appspot.com') {
+  		console.log('STAGING FIREBASE');
+		var config = {
+			apiKey: "AIzaSyCUlM-SNrfMuv9jO0XGpwdNANPj0eq1He0",
+			authDomain: "golf-meta-staging.firebaseapp.com",
+			databaseURL: "https://golf-meta-staging.firebaseio.com",
+			projectId: "golf-meta-staging",
+			storageBucket: "golf-meta-staging.appspot.com",
+			messagingSenderId: "1052400024871"
+		};
+
+	}
   firebase.initializeApp(config);
